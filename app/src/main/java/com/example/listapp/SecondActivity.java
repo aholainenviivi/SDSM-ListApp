8651f72d14bb35c18f2aac0dd4e60696b1c9cc8b
aholainenviivi <aholainenviivi@gmail.com>
2022-05-26 01:06:30 +0300

    Changed SecondActivity to show corresponding icon scaled up to fit the screen.

--- app/src/main/java/com/example/listapp/SecondActivity.java
+++ app/src/main/java/com/example/listapp/SecondActivity.java
@@ -3,7 +3,11 @@ package com.example.listapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Display;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
@@ -16,8 +20,27 @@ public class SecondActivity extends AppCompatActivity {
         Intent intent = getIntent();
         int itemIndex = intent.getIntExtra("itemIndex", -1);
 
-        String[] categories = getResources().getStringArray(R.array.categories);
-        TextView secondActivityTextView = findViewById(R.id.secondActivityTextView);
-        secondActivityTextView.setText("You clicked on " + categories[itemIndex]);
+        int icons[] = {R.drawable.ironman_icon, R.drawable.ultimate_ironman_icon, R.drawable.hardcore_ironman_icon};
+
+        ImageView secondActivityImageView = findViewById(R.id.secondActivityImageView);
+        scaleImage(secondActivityImageView, icons[itemIndex]);
     }
+
+    private void scaleImage(ImageView img, int icon) {
+        Display display = getWindowManager().getDefaultDisplay();
+        BitmapFactory.Options options = new BitmapFactory.Options();
+
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(), icon, options);
+
+        int imgWidth = options.outWidth;
+        int displayWidth = display.getWidth();
+        int ratio = Math.round((float) imgWidth / (float) displayWidth);
+        options.inSampleSize = ratio;
+
+        options.inJustDecodeBounds = false;
+        Bitmap scaledImg = BitmapFactory.decodeResource(getResources(), icon, options);
+        img.setImageBitmap(scaledImg);
+    }
+
 }
